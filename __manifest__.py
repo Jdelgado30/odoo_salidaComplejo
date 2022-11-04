6a2ef19fa1630b5dfe7edbe53f99ab8bebc94684
Jdelgado30 <josedelg1994@gmail.com>
2022-11-03 20:43:10 -0400

    chatter

--- __manifest__.py
+++ __manifest__.py
@@ -3,11 +3,11 @@
     "name": "Salidacomplejo",
     "description": "Modulo para salida del complejo",
     "data": [
-        'security/salidacomplejo_security.xml',
-        'security/ir.model.access.csv',
         'views/salidacomplejo_salida_views.xml',
         'views/salidacomplejo_views.xml',
+        'security/salidacomplejo_security.xml',
+        'security/ir.model.access.csv',
     ],
-    "depends": ['base'],
+    "depends": ['base','hr','contacts','mail'],
     "application": "True"
 }
--- models/salidacomplejo_salida.py
+++ models/salidacomplejo_salida.py
@@ -2,5 +2,7 @@ from odoo import fields, models
 
 class salida(models.Model):
     _name = "salidacomplejo.salida"
-    name = fields.Char(string="secuencia",readonly="True")
+    _inherit= ['mail.thread','mail.activity.mixin']
+    name = fields.Char(string="secuencia",readonly="True",default="New")
+    contacts_id =fields.Many2one(comodel_name="res.user",string="Contacto")
 
