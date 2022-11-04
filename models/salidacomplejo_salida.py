3e8838e9681606e6c8f8a6cb3cf85411046c22ea
Jdelgado30 <josedelg1994@gmail.com>
2022-11-03 23:48:07 -0400

    sequence

--- models/salidacomplejo_salida.py
+++ models/salidacomplejo_salida.py
@@ -1,8 +1,15 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class salida(models.Model):
     _name = "salidacomplejo.salida"
     _inherit= ['mail.thread','mail.activity.mixin']
     name = fields.Char(string="secuencia",readonly="True",default="New")
-    contacts_id =fields.Many2one(comodel_name="res.user",string="Contacto")
+    contacts_id =fields.Many2one(comodel_name="res.partner",string="Contacto")
+    direccion = fields.Char(related="contacts_id.contact_address",string="Direccion")
 
+    @api.model
+    def create(self,vals):
+        if vals.get('name','New') == "New":
+            vals['name']=self.env['ir.sequence'].next_by_code('code_sc_1') or "New"
+        result = super(salida,self).create(vals)
+        return result
