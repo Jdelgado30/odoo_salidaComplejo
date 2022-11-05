f75508a6bb5c9096b452b4fc0fc8ba79b4f9f813
Jdelgado30 <josedelg1994@gmail.com>
2022-11-04 22:16:09 -0400

    campos relacionados

--- models/salidacomplejo_salida.py
+++ models/salidacomplejo_salida.py
@@ -2,11 +2,14 @@ from odoo import fields, models, api
 
 class salida(models.Model):
     _name = "salidacomplejo.salida"
+    _description = "Salida del Complejo"
     _inherit= ['mail.thread','mail.activity.mixin']
     name = fields.Char(string="secuencia",readonly="True",default="New")
-    contacts_id =fields.Many2one(comodel_name="res.partner",string="Contacto")
+    contacts_id =fields.Many2one(comodel_name="res.partner",string="Destino")
     direccion = fields.Char(related="contacts_id.contact_address",string="Direccion")
-
+    solicitante = fields.Many2one(comodel_name="hr.employee", string="Solicitante")
+    cargo = fields.Char(related="solicitante.job_id.display_name", string="Cargo")
+    empresa = fields.Many2one(comodel_name="res.company", string="Compañia")
     @api.model
     def create(self,vals):
         if vals.get('name','New') == "New":
